86300f1eb0cabc1a8a9cdba223a7eda5d7022924
MostafaMohamed2001 <mostafamohamed8828@gmail.com>
2026-04-15 02:41:43 +0200

    Refactor Docker Task4 structure by removing obsolete files and resources for improved organization.

---
+++ Docker/Task6/app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+port=8080
+
+@app.route('/')
+def show_env():
+    mode = os.getenv("APP_MODE", "default")
+    region = os.getenv("APP_REGION", "unknown")
+    return f"App mode: {mode}, Region: {region}"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
+
